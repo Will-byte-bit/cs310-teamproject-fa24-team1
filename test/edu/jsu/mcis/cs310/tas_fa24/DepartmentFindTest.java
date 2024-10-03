54afc20afd00a4290fc14ed01ed3606030d52840
SamCain <scain6@stu.jsu.edu>
2024-10-03 13:58:54 -0500

    Added getter to DAOFactory and added some tests to DepartmentFindTest.java and EmployeeFindTest.java

--- test/edu/jsu/mcis/cs310/tas_fa24/DepartmentFindTest.java
+++ test/edu/jsu/mcis/cs310/tas_fa24/DepartmentFindTest.java
@@ -59,5 +59,28 @@ public class DepartmentFindTest {
         assertEquals("#8 (Shipping), Terminal ID: 107", d3.toString());
 
     }
+    
+    @Test
+    public void testFindDepartment4() {
+	    DepartmentDAO departmentDAO = daoFactory.getDepartmentDAO();
+	    
+	    // Retrieve Department from Database
+	    Department d4 = departmentDAO.find(7);
+	    
+	    // Compare to Expected Value
+	    assertEquals("#7 (Press), Terminal ID: 104", d4.toString());
+    }
+    
+    @Test
+    public void testFindDepartment5() {
+	    DepartmentDAO departmentDAO = daoFactory.getDepartmentDAO();
+	    
+	    // Retrieve Department from Database
+	    Department d5 = departmentDAO.find(2);
+	    
+	    // Compare to Expected Value
+	    assertEquals("#2 (Cleaning), Terminal ID: 107", d5.toString());
+    }
+
 
 }
