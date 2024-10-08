a3f121e5bcbe4993a2399d8f65429e4b1e6d0249
Josh Whaley <jwhaley4@stu.jsu.edu>
2024-10-08 12:49:56 -0500

    Adjustment to alt test name.

--- src/edu/jsu/mcis/cs310/tas_fa24/dao/DepartmentDAO.java
+++ src/edu/jsu/mcis/cs310/tas_fa24/dao/DepartmentDAO.java
@@ -1,7 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package edu.jsu.mcis.cs310.tas_fa24.dao;
 
 import edu.jsu.mcis.cs310.tas_fa24.Department;
@@ -10,6 +6,11 @@ import java.sql.*;
 /**
  *
  * @author joshwhaley
+ * 
+ * This class makes use of the daoFactory in order to create a find() method for
+ * the department, taking the department id as a parameter, which will then 
+ * return all the information of the department. Additionally, I added a method
+ * that finds the department based off the terminal id. 
  */
 public class DepartmentDAO {
     
@@ -62,7 +63,52 @@ public class DepartmentDAO {
                 }
         }
  
-        return department;        
+        return department;
+    }
+    }
+        
+        
+    // find department method based off terminalID
+    // find a way to make this method reachable
+    public Department findByTermID(int terminalID){
+        
+        Department department = null;
+        String QueryFindTermID = "SELECT * FROM department WHERE terminalid = ?";
+        ResultSet resultSet = null;
+        
+        try(PreparedStatement ps = daoFactory.getConnection().prepareStatement(QueryFindTermID)){
+            
+            // get deptID
+            ps.setInt(1, terminalID);
+            resultSet = ps.executeQuery();
+            
+            // retrieve fields from resultSet
+            if(resultSet.next()){
+                
+                String description = resultSet.getString("description");
+                int deptID = resultSet.getInt("id");
+                department = new Department(deptID, description, terminalID);
+            
+            }
+            
+            ps.close();
+            
+        } catch (SQLException e) {
+
+            throw new DAOException(e.getMessage());
+
+        } finally {
+
+            if (resultSet != null) {
+                try {
+                    resultSet.close();
+                } catch (SQLException e) {
+                    throw new DAOException(e.getMessage());
+                }
+        
+        }
+        
+        return department;
     }
      
     
