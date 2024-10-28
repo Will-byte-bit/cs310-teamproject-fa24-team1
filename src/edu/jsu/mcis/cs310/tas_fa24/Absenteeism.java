69518196825256a3adc95f9e8c6a794b0dcf2410
SamCain <scain6@stu.jsu.edu>
2024-10-28 11:14:40 -0500

    Added Absenteeism Model and DAO classes

---
+++ src/edu/jsu/mcis/cs310/tas_fa24/Absenteeism.java
@@ -0,0 +1,9 @@
+package edu.jsu.mcis.cs310.tas_fa24;
+
+/**
+ *
+ * @author samca
+ */
+public class Absenteeism {
+
+}
---
+++ src/edu/jsu/mcis/cs310/tas_fa24/dao/AbsenteeismDAO.java
@@ -0,0 +1,9 @@
+package edu.jsu.mcis.cs310.tas_fa24.dao;
+
+/**
+ *
+ * @author samca
+ */
+public class AbsenteeismDAO {
+
+}
