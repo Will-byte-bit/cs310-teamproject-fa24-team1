c5267451a0e8f73755c7649c513e641827928ff1
William Saint <wsaint@stu.jsu.edu>
2024-11-13 18:23:39 -0600

    Finished Whos in whos out

--- src/edu/jsu/mcis/cs310/tas_fa24/dao/ReportDAO.java
+++ src/edu/jsu/mcis/cs310/tas_fa24/dao/ReportDAO.java
@@ -3,12 +3,22 @@ package edu.jsu.mcis.cs310.tas_fa24.dao;
 import com.github.cliftonlabs.json_simple.JsonArray;
 import com.github.cliftonlabs.json_simple.JsonObject;
 import com.github.cliftonlabs.json_simple.Jsoner;
+import edu.jsu.mcis.cs310.tas_fa24.EmployeeType;
+import edu.jsu.mcis.cs310.tas_fa24.Shift;
+import edu.jsu.mcis.cs310.tas_fa24.dao.ShiftDAO;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Timestamp;
 import java.text.SimpleDateFormat;
+import java.time.DayOfWeek;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.TemporalAdjusters;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 /**
@@ -167,5 +177,184 @@ public class ReportDAO {
     JsonArray jsonArray = new JsonArray();
     jsonArray.addAll(employeeSummaryList);
     return Jsoner.prettyPrint(jsonArray.toJson());
+    }
+    public String getWhosInWhosOut(LocalDateTime time, Integer departmentID){
+        time.withNano(0);
+        
+        JsonArray result = new JsonArray();
+       
+        
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+        DateTimeFormatter formatterForRecord = DateTimeFormatter.ofPattern("MM/dd/yyyy HH:mm:ss");
+        
+        LocalDateTime beginningOfDay = LocalDateTime.of(time.toLocalDate(), LocalTime.of(0,0));
+
+        Timestamp end = Timestamp.valueOf(time.format(formatter));
+        Timestamp begin = Timestamp.valueOf(beginningOfDay.format(formatter));
+        
+        String query = "";
+        ResultSet rs = null;
+        PreparedStatement ps = null;
+        try{
+            Connection conn = daoFactory.getConnection();
+
+            if(departmentID != null){
+                query = "WITH new_table AS (\n" +
+                        "    SELECT \n" +
+                        "        employee.badgeid AS newbadgeid,\n" +
+                        "        employee.employeetypeid,\n" +
+                        "        employee.lastname,\n" +
+                        "        employee.firstname, \n" +
+                        "        employee.shiftid,\n" +
+                        "        employee.departmentid,\n" +
+                        "        event.eventtypeid,\n" +
+                        "        event.timestamp\n" +
+                        "    FROM employee\n" +
+                        "    INNER JOIN event \n" +
+                        "        ON employee.badgeid = event.badgeid\n" +
+                        "    WHERE employee.departmentid = ?\n" +
+                        "      AND event.eventtypeid != 2\n" +
+                        "      AND event.timestamp >= ?\n" +
+                        "      AND event.timestamp <= ?\n" +
+                        ")\n" +
+                        "select * from new_table\n" +
+                        "UNION ALL\n" +
+                        " SELECT \n" +
+                        "    employee.badgeid,\n" +
+                        "    employee.employeetypeid,\n" +
+                        "    employee.lastname,\n" +
+                        "    employee.firstname, \n" +
+                        "    employee.shiftid,\n" +
+                        "    employee.departmentid,\n" +
+                        "    0 as timestamp, \n" +
+                        "    0 as eventtypeid\n" +
+                        "FROM employee\n" +
+                        "WHERE employee.departmentid = ?\n" +
+                        "  AND employee.badgeid NOT IN (\n" +
+                        "        SELECT newbadgeid\n" +
+                        "        FROM new_table\n" +
+                        "    )\n" +
+                        "ORDER BY eventtypeid desc, employeetypeid DESC, lastname, firstname;";
+                        ps = conn.prepareStatement(query);
+                        ps.setInt(1, departmentID);
+                        ps.setTimestamp(2, begin);
+                        ps.setTimestamp(3, end);
+                        ps.setInt(4, departmentID);
+                        
+                        rs = ps.executeQuery();
+                        StringBuilder sb = new StringBuilder();
+                        while(rs.next()){
+                            JsonObject record = new JsonObject();
+                            ShiftDAO shiftDAO = new ShiftDAO(this.daoFactory);
+                            Shift shift = shiftDAO.find(rs.getInt("shiftid"));
+                            
+                            if(rs.getInt("eventtypeid") == 1){
+                                LocalDateTime arrived = rs.getTimestamp("timestamp").toLocalDateTime().withNano(0);
+                                String day = arrived.getDayOfWeek().toString().substring(0, 3).concat(" ").concat(arrived.format(formatterForRecord));
+                                record.put("arrived", day);
+                            }
+                           
+                            if(rs.getInt("employeetypeid") == 1){
+                                record.put("employeetype", "Full-Time Employee");
+                            }
+                            else if (rs.getInt("employeetypeid") == 0){
+                                record.put("employeetype", "Temporary Employee");
+                            }
+                            record.put("firstname", rs.getString("firstname"));
+                            record.put("badgeid", rs.getString("newbadgeid"));
+                            record.put("shift", shift.getDescription());
+                            record.put("lastname", rs.getString("lastname"));
+                            if(rs.getInt("eventtypeid") == 1 || rs.getInt("eventtypeid") == 2){
+                                record.put("status", "In");
+                               
+                            }
+                            else if(rs.getInt("eventtypeid") == 0){
+                                record.put("status", "Out");
+                            }
+                            result.add(record);
+                        }
+                        
+            }else{
+                query = "WITH new_table AS (\n" +
+                        "    SELECT \n" +
+                        "        employee.badgeid AS newbadgeid,\n" +
+                        "        employee.employeetypeid,\n" +
+                        "        employee.lastname,\n" +
+                        "        employee.firstname, \n" +
+                        "        employee.shiftid,\n" +
+                        "        employee.departmentid,\n" +
+                        "        event.eventtypeid,\n" +
+                        "        event.timestamp\n" +
+                        "    FROM employee\n" +
+                        "    INNER JOIN event \n" +
+                        "        ON employee.badgeid = event.badgeid\n" +
+                        "      AND event.eventtypeid != 2\n" +
+                        "      AND event.timestamp >= ?\n" +
+                        "      AND event.timestamp <= ?\n" +
+                        ")\n" +
+                        "select * from new_table\n" +
+                        "UNION ALL\n" +
+                        "\n" +
+                        "SELECT \n" +
+                        "    employee.badgeid,\n" +
+                        "    employee.employeetypeid,\n" +
+                        "    employee.lastname,\n" +
+                        "    employee.firstname, \n" +
+                        "    employee.shiftid,\n" +
+                        "    employee.departmentid,\n" +
+                        "    0 as timestamp, \n" +
+                        "    0 as eventtypeid\n" +
+                        "FROM employee \n" +
+                        "where employee.badgeid NOT IN (\n" +
+                        "        SELECT newbadgeid\n" +
+                        "        FROM new_table\n" +
+                        "    )\n" +
+                        "ORDER BY eventtypeid desc, employeetypeid DESC, lastname, firstname;";
+                         ps = conn.prepareStatement(query);
+             
+                        ps.setTimestamp(1, begin);
+                        ps.setTimestamp(2, end);
+                        
+                        rs = ps.executeQuery();
+                        while(rs.next()){
+                            
+                            
+                           
+                            JsonObject record = new JsonObject();
+                            ShiftDAO shiftDAO = new ShiftDAO(this.daoFactory);
+                            Shift shift = shiftDAO.find(rs.getInt("shiftid"));
+                             if(rs.getInt("eventtypeid") == 1){
+                                LocalDateTime arrived = rs.getTimestamp("timestamp").toLocalDateTime().withNano(0);
+                                String day = arrived.getDayOfWeek().toString().substring(0, 3).concat(" ").concat(arrived.format(formatterForRecord));
+                                record.put("arrived", day);
+                            }
+                            if(rs.getInt("employeetypeid") == 1){
+                                record.put("employeetype", "Full-Time Employee");
+                            }
+                            else if (rs.getInt("employeetypeid") == 0){
+                                record.put("employeetype", "Temporary Employee");
+                            }
+                            record.put("firstname", rs.getString("firstname"));
+                            record.put("badgeid", rs.getString("newbadgeid"));
+                            record.put("shift", shift.getDescription());
+                            record.put("lastname", rs.getString("lastname"));
+                            
+                            if(rs.getInt("eventtypeid") == 1){
+                                record.put("status", "In");
+                            }
+                            else if (rs.getInt("eventtypeid") == 0){
+                                
+                                record.put("status", "Out");
+                            }
+                            result.add(record);
+                        }
+
+            }
+            
+        }catch (SQLException e) {
+            throw new DAOException("Error retrieving whos in whos out: " + e.getMessage());
+        }
+        System.out.println(Jsoner.serialize(result));
+        return Jsoner.serialize(result);
     }
 }
